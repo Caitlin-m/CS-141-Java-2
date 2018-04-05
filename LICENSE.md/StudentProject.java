62653c5e3995b323091568bddd8e9a8d169c59c5
Caitlin McMurchie <c.mcmurchie1334@edmail.edcc.edu>
2018-04-05 11:28:56 -0700

    Create StudentProject.java

---
+++ LICENSE.md/StudentProject.java
@@ -0,0 +1,8 @@
+package mcmurchie;
+
+public class StudentProject {
+
+    public static void main(String[] args) {
+        Student student1 = new Student("Flintstone", "Fred", 789456123);
+    }
+}
